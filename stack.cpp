87da0aea46a989402a9ba9bcebe2aa9b4c9d582d
armarts <olympg@yandex.ru>
2017-03-03 20:24:42 +0300

    update version of stack

--- stack.cpp
+++ stack.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <cassert>
+#include <cstdio>
 #include "stack.h"
+#include <string>
 
 using namespace std;
 
 int main() {
-		Stack s(0);
-		s.push(5);
-		//s.pop();
-		s.dump();
+	Stack<int> s(1);
+	s.push(1);
+	s.dump();
+	s.pop();
+	s.pop();	
 	return 0;
 }
-
